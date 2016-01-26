01bed07f50a7b3f3224da676485e9b8461c9e330
zhouyibing <zhouyibing.sh@superjia.com>
2016-01-26 18:00:26 +0800

    volatileLock

---
+++ src/main/java/zhou/concurrent/volatiletest/VolatileLock.java
@@ -0,0 +1,54 @@
+package zhou.concurrent.volatiletest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Zhou Yibing on 2016/1/26.
+ * 基于volatile来实现一个lock,不太可靠
+ */
+public class VolatileLock {
+    private static final Lock lock = new Lock();
+   public static void main(String[] args){
+       ExecutorService executorService = Executors.newFixedThreadPool(10);
+       for(int i=0;i<10;i++) {
+           executorService.submit(new Runnable() {
+               @Override
+               public void run() {
+                   try {
+                       lock.lock();
+                       System.out.println(Thread.currentThread().getName() + " is working...");
+                       Thread.sleep(1000);
+                   } catch (InterruptedException e){
+                       e.printStackTrace();
+                   }finally {
+                       lock.unlock();
+                   }
+               }
+           });
+       }
+       try {
+           executorService.awaitTermination(1, TimeUnit.MINUTES);
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+   }
+
+
+    public static class Lock{
+        volatile  boolean flag=false;
+        public void lock(){
+            System.out.println(Thread.currentThread().getName() + " wait to get the lock.");
+            while (flag){
+            }
+            flag = true;
+            System.out.println(Thread.currentThread().getName() + " has get the lock.");
+        }
+
+        public void unlock(){
+            System.out.println(Thread.currentThread().getName()+" has release the lock.");
+            flag = false;
+        }
+    }
+}
