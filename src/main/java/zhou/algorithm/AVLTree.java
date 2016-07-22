476b74ab1b134500e367d927d2afc7d83e965b3a
yibingzhou <yibingzhou@ctrip.com>
2016-07-22 19:48:03 +0800

    algorithm

---
+++ src/main/java/zhou/algorithm/AVLTree.java
@@ -0,0 +1,41 @@
+package zhou.algorithm;
+
+import lombok.Data;
+
+/**
+ * Created by yibingzhou on 2016/7/21.
+ * 平衡二叉树
+ *一棵AVL树满足以下的条件:
+ 1>它的左子树和右子树都是AVL树
+ 2>左子树和右子树的高度差不能超过1
+ 性质:
+ 1>一棵n个结点的AVL树的其高度保持在0(log2(n)),不会超过3/2log2(n+1)
+ 2>一棵n个结点的AVL树的平均搜索长度保持在0(log2(n)).
+ 3>一棵n个结点的AVL树删除一个结点做平衡化旋转所需要的时间为0(log2(n)).
+
+ LL型 data<unbalanceNode.data&&data<unbalanceNode.left.data
+ RR型 data>unbalanceNode.data&&data>unbalanceNode.left.data
+ LR型 data<unbalanceNode.data&&data>unbalanceNode.left.data
+ RL型 data>unbalanceNode.data&&data<unbalanceNode.right.data
+ */
+public class AVLTree<T extends Comparable>{
+    private Node root;
+    private int size;
+
+    private void add(T v){
+
+    }
+
+    @Data
+    class Node<T>{
+        private T value;
+        private Node parent;
+        private Node left;
+        private Node right;
+        private int bf;//平衡因子
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+}
--- src/main/java/zhou/algorithm/BSTree.java
+++ src/main/java/zhou/algorithm/BSTree.java
@@ -1,5 +1,10 @@
 package zhou.algorithm;
 
+import com.google.common.collect.Lists;
+import lombok.Data;
+
+import java.util.List;
+
 /**
  * Created by yibingzhou on 2016/7/20.
  * 二叉查找树的java实现
@@ -10,5 +15,240 @@ package zhou.algorithm;
  （3）左、右子树也分别为二叉排序树；
  （4）没有键值相等的结点
  */
-public class BSTree {
+public class BSTree<T extends Comparable> {
+    private Node root;
+    private int size;
+
+    public void add(T v){
+        Node n = new Node(v);
+        if(null==root) {
+            root=n;
+            size++;
+            return;
+        }
+        Node current = root;
+        while(true) {
+            if (current.value.compareTo(n.value) > 0) {
+                //插入左子树
+                if(current.left==null) {
+                    current.left = n;
+                    n.parent=current;
+                    size++;
+                    break;
+                }
+                else
+                    current = current.left;
+            } else {
+                //插入右子树
+                if(current.right==null) {
+                    current.right = n;
+                    n.parent=current;
+                    size++;
+                    break;
+                }
+                else
+                    current = current.right;
+            }
+        }
+    }
+
+    public void delete(T v){
+        Node current = root;
+        while (null!=current){
+            int r = current.value.compareTo(v);
+            if(r>0)
+                current = current.left;
+            else if(r<0)
+                current = current.right;
+            else{
+                //将左子树最大的节点，替换当前节点。或将右子树的最小节点替换当前节点
+                Node temp = null;
+                if(current.left!=null){
+                    temp=maxNode(current.left);
+                }else if(current.right!=null){
+                    temp=maxNode(current.right);
+                }
+                if(null!=current.parent&&current.parent.left==current) current.parent.left= temp;
+                else if(null!=current.parent&&current.parent.right==current) current.parent.right=temp;
+                if(temp!=null){
+                    if(temp!=current.left) {
+                        temp.left = current.left;
+                        current.left.parent = temp;
+                    }
+                    if(temp!=current.right) {
+                        temp.right = current.right;
+                        current.right.parent = temp;
+                    }
+                    if(temp.value.compareTo(current.value)<0)
+                       temp.parent.right=null;
+                    if(temp.value.compareTo(current.value)>0)
+                        temp.parent.left=null;
+                }
+                current=null;
+            }
+        }
+    }
+
+    private Node maxNode(Node n){
+        if(null==n)
+            return n;
+        if(n.right==null)
+            return n;
+        else
+            return maxNode(n.right);
+    }
+
+    private Node minNode(Node n){
+        if(null==n)
+            return n;
+        if(n.left==null)
+            return n;
+        else
+            return minNode(n);
+    }
+
+    public Node findFirst(T v){
+        Node current = root;
+        while(current!=null) {
+            int com = current.value.compareTo(v);
+            if (com == 0)
+                return current;
+            else if(com>0)
+                current=current.left;
+            else
+                current=current.right;
+        }
+        return null;
+    }
+
+    /**
+     * 获得节点的直接前驱节点（比指点节点小的最大的值）
+     * @param v
+     * @return
+     */
+    public T preNode(T v){
+        if(root==null) return null;
+        Node current = root;
+        while (true) {
+            int com = current.value.compareTo(v);
+            if (com == 0) {
+                if (current.left == null) return null;
+                return current.left.value;
+            } else if (com > 0) {
+                if (current.right == null) return null;
+                current = current.right;
+            } else {
+                if (current.left == null) return null;
+                current = current.left;
+            }
+        }
+    }
+
+    /**
+     * 获得节点的直接后驱节点（比指定节点大的最小的值）
+     * @param v
+     * @return
+     */
+    public T postNode(T v){
+        if(root==null) return null;
+        Node current = root;
+        while (true) {
+            int com = current.value.compareTo(v);
+            if (com == 0) {
+                if (current.right == null) return null;
+                return current.right.value;
+            } else if (com > 0) {
+                if (current.left == null) return null;
+                current = current.left;
+            } else {
+                if (current.right == null) return null;
+                current = current.right;
+            }
+        }
+    }
+    /**
+     * 前序排序
+     * @return
+     */
+    public List<T> preSort(){
+        if(root==null) return null;
+        List<T> list = Lists.newArrayListWithExpectedSize(size);
+        preSort(root,list);
+        return list;
+    }
+
+    private void preSort(Node n, List<T> list){
+        list.add(n.value);
+        if(n.left!=null)
+            preSort(n.left,list);
+        if(n.right!=null)
+            preSort(n.right,list);
+    }
+
+    /**
+     * 中序排序
+     * @return
+     */
+    public List<T> midSort(){
+        if(root==null) return null;
+        List<T> list = Lists.newArrayListWithExpectedSize(size);
+        midSort(root,list);
+        return list;
+    }
+
+    private void midSort(Node n, List<T> list){
+        if(n.left!=null)
+            midSort(n.left,list);
+        list.add(n.value);
+        if(n.right!=null)
+            midSort(n.right,list);
+    }
+
+    /**
+     * 后序排序
+     * @return
+     */
+    public List<T> postSort(){
+        if(root==null) return null;
+        List<T> list = Lists.newArrayListWithExpectedSize(size);
+        postSort(root,list);
+        return list;
+    }
+
+    private void postSort(Node n, List<T> list){
+        if(n.left!=null)
+            postSort(n.left,list);
+        if(n.right!=null)
+            postSort(n.right,list);
+        list.add(n.value);
+    }
+
+    @Data
+    class Node{
+        private T value;
+        private Node parent;
+        private Node left;
+        private Node right;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args){
+        BSTree<Long> tree = new BSTree<>();
+        tree.add(12L);
+        tree.add(23234L);
+        tree.add(225L);
+        tree.add(2L);
+        tree.add(356L);
+        tree.add(14L);
+        tree.add(258L);
+        tree.add(123L);
+        tree.delete(225L);
+        List<Long> midSort = tree.midSort();
+        for(Long i:midSort)
+            System.out.print(i+",");
+        System.out.println(tree.findFirst(258L));
+    }
 }
